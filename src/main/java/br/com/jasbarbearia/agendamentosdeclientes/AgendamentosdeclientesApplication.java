6d8c0594df377d66f22832f01dc044c1073c1d6d
Josivan Araujo <josivanaraujodesantana@gmail.com>
2025-09-14 23:20:24 -0300

    Adiciona README.md inicial

---
+++ src/main/java/br/com/jasbarbearia/agendamentosdeclientes/AgendamentosdeclientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.jasbarbearia.agendamentosdeclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentosdeclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentosdeclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jasbarbearia/agendamentosdeclientes/AgendamentosdeclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jasbarbearia.agendamentosdeclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentosdeclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
